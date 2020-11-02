578529d01daf587c8f64c89d72bf3b33db8590a0
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 17:24:12 +0200

    feat: build search-term-context & assign dashboard-compound as provider

--- app/Compounds/DashboardCompound.js
+++ app/Compounds/DashboardCompound.js
@@ -1,15 +1,20 @@
-import React from "react"
-import DisplayCompound from './DisplayCompound'
+import React, { useState } from "react"
+import DisplayCompound from "./DisplayCompound"
 import ControlsCompound from "./ControlsCompound"
+import { SearchTermContext } from "../Context/SearchTermContext"
 
 export default DashboardCompound
 
 function DashboardCompound({ children }) {
+  const [searchTerm, setSearchTerm] = useState("")
+
   return (
     <>
-    <ControlsCompound />
-    <DisplayCompound />
-    {children}  
+      <SearchTermContext.Provider value={[searchTerm, setSearchTerm]}>
+        <ControlsCompound />
+        <DisplayCompound />
+        {children}
+      </SearchTermContext.Provider>
     </>
   )
 }
