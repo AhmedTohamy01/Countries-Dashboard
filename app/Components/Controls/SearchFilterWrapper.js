1737c3e867ad35f56a9abb802c56d88a9b31513f
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 04:19:26 +0200

    refactor: build search-filter-wrapper and add it in controls-compound

---
+++ app/Components/Controls/SearchFilterWrapper.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Box, Paper } from "@material-ui/core"
+
+export default SearchFilterWrapper
+
+function SearchFilterWrapper({ children, ...restProps }) {
+  return (
+    <Box component={Paper} elevation={10} p={4} width="100%" {...restProps}>
+      {children}
+    </Box>
+  )
+}
--- app/Compounds/ControlsCompound.js
+++ app/Compounds/ControlsCompound.js
@@ -1,5 +1,7 @@
 import React from "react"
 import ControlsSectionWrapper from '../Components/Controls/ControlsSectionWrapper'
+import FiltersWrapper from '../Components/Controls/FiltersWrapper'
+import SearchFilterWrapper from '../Components/Controls/SearchFilterWrapper'
 
 export default ControlsCompound
 
@@ -7,7 +9,11 @@ function ControlsCompound({ children }) {
   return (
     <>
       <ControlsSectionWrapper>
-        
+        <FiltersWrapper>
+          <SearchFilterWrapper>
+            
+          </SearchFilterWrapper>
+        </FiltersWrapper>
       </ControlsSectionWrapper>
       {children}
     </>
