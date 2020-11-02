c45803a3441a171285b05f9d9c0f0384057ce1c4
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 15:57:23 +0200

    refactor: build region-filter-wrapper & add it in controls compoound

--- app/Compounds/ControlsCompound.js
+++ app/Compounds/ControlsCompound.js
@@ -2,11 +2,13 @@ import React from "react"
 import CheckBox from '../Components/Controls/CheckBox'
 import ControlsSectionWrapper from '../Components/Controls/ControlsSectionWrapper'
 import FiltersWrapper from '../Components/Controls/FiltersWrapper'
+import RegionFilterWrapper from '../Components/Controls/RegionFilterWrapper'
 import SearchCheckBoxesWrapper from '../Components/Controls/SearchCheckBoxesWrapper'
 import SearchField from '../Components/Controls/SearchField'
 import SearchFieldWrapper from '../Components/Controls/SearchFieldWrapper'
 import SearchFilterWrapper from '../Components/Controls/SearchFilterWrapper'
 import Text from "../Components/Controls/Text"
+import { Grid, Box } from "@material-ui/core"
 
 export default ControlsCompound
 
@@ -15,17 +17,22 @@ function ControlsCompound({ children }) {
     <>
       <ControlsSectionWrapper>
         <FiltersWrapper>
-          <SearchFilterWrapper>
-            <Text>Search Filter</Text>
-            <SearchFieldWrapper>
-              <SearchField />
-            </SearchFieldWrapper>
-            <SearchCheckBoxesWrapper>
-              <CheckBox id="name" label="Name" />
-              <CheckBox id="capital" label="Capital" />
-              <CheckBox id="langauge" label="Language" />
-            </SearchCheckBoxesWrapper>
-          </SearchFilterWrapper>
+          <Grid item>
+            <SearchFilterWrapper>
+              <Text>Search Filter</Text>
+              <SearchFieldWrapper>
+                <SearchField />
+              </SearchFieldWrapper>
+              <SearchCheckBoxesWrapper>
+                <CheckBox id="name" label="Name" />
+                <CheckBox id="capital" label="Capital" />
+                <CheckBox id="langauge" label="Language" />
+              </SearchCheckBoxesWrapper>
+            </SearchFilterWrapper>
+          </Grid>
+          <Grid item>
+            <RegionFilterWrapper></RegionFilterWrapper>
+          </Grid>
         </FiltersWrapper>
       </ControlsSectionWrapper>
       {children}
