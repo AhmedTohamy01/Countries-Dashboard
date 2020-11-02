5fcd455b1bd0780387f7450c992be12b0360263e
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 04:09:53 +0200

    feat: build controls-compound and add it in dashboard compound

---
+++ app/Compounds/ControlsCompound.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default ControlsCompound
+
+function ControlsCompound({ children }) {
+  return (
+    <>
+      controls compound
+      {children}
+    </>
+  )
+}
--- app/Compounds/DashboardCompound.js
+++ app/Compounds/DashboardCompound.js
@@ -1,11 +1,13 @@
 import React from "react"
 import DisplayCompound from './DisplayCompound'
+import ControlsCompound from "./ControlsCompound"
 
 export default DashboardCompound
 
 function DashboardCompound({ children }) {
   return (
     <>
+    <ControlsCompound />
     <DisplayCompound />
     {children}  
     </>
