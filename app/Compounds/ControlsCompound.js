2d783c210b111bc3e60af88fc421eafcbf7bba21
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 15:43:09 +0200

    refactor: build search-field-wrapper and add it in controls compound

--- app/Compounds/ControlsCompound.js
+++ app/Compounds/ControlsCompound.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ControlsSectionWrapper from '../Components/Controls/ControlsSectionWrapper'
 import FiltersWrapper from '../Components/Controls/FiltersWrapper'
+import SearchFieldWrapper from '../Components/Controls/SearchFieldWrapper'
 import SearchFilterWrapper from '../Components/Controls/SearchFilterWrapper'
 import Text from "../Components/Controls/Text"
 
@@ -13,7 +14,9 @@ function ControlsCompound({ children }) {
         <FiltersWrapper>
           <SearchFilterWrapper>
             <Text>Search Filter</Text>
+            <SearchFieldWrapper>
 
+            </SearchFieldWrapper>
           </SearchFilterWrapper>
         </FiltersWrapper>
       </ControlsSectionWrapper>
