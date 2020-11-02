e567b3449f8601adfd8f162da959933d98be5cee
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 15:46:44 +0200

    refactor: build search-checkboxes-wrapper & add it in controls compound

--- app/Compounds/ControlsCompound.js
+++ app/Compounds/ControlsCompound.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ControlsSectionWrapper from '../Components/Controls/ControlsSectionWrapper'
 import FiltersWrapper from '../Components/Controls/FiltersWrapper'
+import SearchCheckBoxesWrapper from '../Components/Controls/SearchCheckBoxesWrapper'
 import SearchField from '../Components/Controls/SearchField'
 import SearchFieldWrapper from '../Components/Controls/SearchFieldWrapper'
 import SearchFilterWrapper from '../Components/Controls/SearchFilterWrapper'
@@ -17,8 +18,10 @@ function ControlsCompound({ children }) {
             <Text>Search Filter</Text>
             <SearchFieldWrapper>
               <SearchField />
-
             </SearchFieldWrapper>
+            <SearchCheckBoxesWrapper>
+              
+            </SearchCheckBoxesWrapper>
           </SearchFilterWrapper>
         </FiltersWrapper>
       </ControlsSectionWrapper>
