01a14e4914c6894dabf44f286a93713e846e78ba
Ahmed Tohamy <Ahmed-Tohamy@outlook.com>
2020-11-02 17:43:23 +0200

    feat: build checkbox-filter-context & assign dashborad-compound as provider

--- app/Compounds/DashboardCompound.js
+++ app/Compounds/DashboardCompound.js
@@ -2,11 +2,13 @@ import React, { useState } from "react"
 import DisplayCompound from "./DisplayCompound"
 import ControlsCompound from "./ControlsCompound"
 import { SearchTermContext } from "../Context/SearchTermContext"
+import { CheckBoxFilterTermContext } from "../Context/CheckBoxFilterTermContext"
 
 export default DashboardCompound
 
 function DashboardCompound({ children }) {
   const [searchTerm, setSearchTerm] = useState("")
+  const [checkBoxFilterTerm, setCheckBoxFilterTerm] = useState("name")
 
   return (
     <>
